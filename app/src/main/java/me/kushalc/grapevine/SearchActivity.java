fe58610d85f518f0b13fd6d3dbeaa9ea381b074a
Kushal <kcutari@gmail.com>
2016-06-13 19:18:18 -0700

    Added ActionBar to search activity

--- app/src/main/java/me/kushalc/grapevine/SearchActivity.java
+++ app/src/main/java/me/kushalc/grapevine/SearchActivity.java
@@ -1,7 +1,12 @@
 package me.kushalc.grapevine;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -9,5 +14,37 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.search_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_profile:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.action_search:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                Intent intent = new Intent(this, SearchActivity.class);
+                startActivity(intent);
+
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar, menu);
+        return true;
     }
 }
